d93360904714a7bd9be3d4f9eae24d8cce9b6b38
akelian <exelsage4@gmail.com>
2020-02-19 00:11:26 +0300

    hw2 v4

---
+++ by.trms.kotlinHW2/src/Launcher.kt
@@ -0,0 +1,12 @@
+class Launcher
+    fun main() {
+        val batya = Father(true, "Active searching","Oleg", 43)
+        batya.say()
+        val spinogriz = Son(false,"Had work", "Oleg2", 18)
+        spinogriz.say()
+        val maman = Mother("Elena", 44, Gender.Woman, 2, "Lonely")
+        maman.say()
+        val dunya = Daughter("Katya", 19, Gender.Woman,0 , "not married")
+        dunya.say()
+
+}
